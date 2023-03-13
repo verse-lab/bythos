08a7048edaa8f05dc4edbab6d90da881e81cf6a6
zqy1018 <zqy1018@sjtu.edu.cn>
2023-03-13 09:09:37 +0800

    abstracted Address

--- Systems/Address.v
+++ Systems/Address.v
@@ -7,8 +7,8 @@ Module Type NetAddr.
 (* how about simply letting addr be a number within some range? any other things to notice here? *)
 (* TODO relate this with Byzantine assumption *)
 
-Definition Address := nat. 
-Definition Address_eqdec := PeanoNat.Nat.eq_dec.
+Parameter Address : Set. 
+Parameter Address_eqdec : forall (a1 a2 : Address), {a1 = a2} + {a1 <> a2}.
 
 Parameter is_byz : Address -> bool.
 Parameter valid_nodes : list Address.
@@ -18,12 +18,22 @@ Parameter t0 : nat.
 
 Axiom t0_lt_N : t0 < N.
 
+(* FIXME: trick *)
+Definition Address_inhabitant : Address.
+  destruct valid_nodes as [ | n ? ] eqn:E.
+  2: exact n.
+  assert (N = 0) as EN by (unfold N; now rewrite -> E).
+  pose proof t0_lt_N.
+  lia.
+Qed.
+
 (* TODO if we want properties about quorum, we need to quantify t0 here *)
 
 Definition valid_node n := In n valid_nodes.
 
 Axiom byz_is_valid : forall n, is_byz n -> valid_node n.
 
+(* this is not used. *)
 Axiom at_least_two_honest : exists n1 n2, 
   n1 <> n2 /\ 
   valid_node n1 /\
