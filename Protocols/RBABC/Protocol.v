cd6a09f622b49a7771e616e1b35c78b16e049dbc
zqy1018 <zqy1018@sjtu.edu.cn>
2024-04-26 21:56:49 +0800

    note

--- Protocols/RBABC/Protocol.v
+++ Protocols/RBABC/Protocol.v
@@ -8,7 +8,7 @@ From ABCProtocol.Protocols.RB Require Import Protocol.
 
 From RecordUpdate Require Import RecordUpdate.
 
-Module Type RBACProtocol (A : NetAddr) (R : Round) (Sn : Signable) (V : SignableValue Sn) (VBFT : ValueBFT A R V) 
+Module Type RBACProtocol (A : NetAddr) (R : Round) (ARP : AddrRoundPair A R) (Sn : Signable) (V : SignableValue Sn) (VBFT : ValueBFT A R V) 
   (BTh : ClassicByzThreshold A) (RBM : RBMessage A R V)
   (P : PKI A Sn) (TSS0 : ThresholdSignatureSchemePrim A Sn with Definition thres := BTh.t0) (* ! *)
   (TSS : ThresholdSignatureScheme A Sn with Module TSSPrim := TSS0)
@@ -20,41 +20,37 @@ Module Type RBACProtocol (A : NetAddr) (R : Round) (Sn : Signable) (V : Signable
   (ACP : ACProtocol A Sn V BTh P TSS0 TSS ACDT CC ACM ACPk)
   (Pk : SimplePacket A M) <: Protocol A M Pk BTh.
 
-Import A R V VBFT BTh P TSS ACDT CC M Pk.
+Import A R ARP V VBFT BTh P TSS ACDT CC M Pk.
 
 (* TODO this should be generic? *)
 Definition pkt_inl (p : RBPk.Packet) : Packet :=
   mkP (RBPk.src p) (RBPk.dst p) (inl (RBPk.msg p)) (RBPk.consumed p).
 
-Definition pkt_inr (ar : Address * Round) (p : ACPk.Packet) : Packet :=
-  mkP (ACPk.src p) (ACPk.dst p) (inr (ar, ACPk.msg p)) (ACPk.consumed p).
+Definition pkt_inr (p : ACPk.Packet) : Packet :=
+  mkP (ACPk.src p) (ACPk.dst p) (inr (ACPk.msg p)) (ACPk.consumed p).
 
 Definition InternalTransition := RBP.InternalTransition.
 
 Record State_ :=
   Node {
     stRB : RBP.State;
-    stAC : Address * Round -> ACP.State;
+    stAC : ACP.State;
   }.
 
 #[export] Instance eta : Settable _ := settable! Node <stRB; stAC>.
 
 Definition State := State_.
 
-Definition Init (n : Address) : State := Node (RBP.Init n) (fun _ => ACP.Init n).
+Definition Init (n : Address) : State := Node (RBP.Init n) (ACP.Init n).
 
 Definition procInt (st : State) (tr : InternalTransition) : State * list Packet :=
   let: (st', pkts) := RBP.procInt st.(stRB) tr in (st <| stRB := st' |>, map pkt_inl pkts).
 
-Definition triggered (st st' : RBP.State) (mRB : RBM.Message) : option (Address * Round * Value) :=
-  match mRB with
-  | RBM.VoteMsg q r _ =>
-    match (st.(RBP.output) (q, r)), (st'.(RBP.output) (q, r)) with
-    | nil, nil => None
-    | _ :: _, _ => None
-    | nil, v :: _ => Some (q, r, v)
-    end
-  | _ => None
+Definition triggered (st st' : RBP.State) : option Value :=
+  match (st.(RBP.output) arp), (st'.(RBP.output) arp) with
+  | nil, nil => None
+  | _ :: _, _ => None
+  | nil, v :: _ => Some v
   end.
 
 Definition procMsgWithCheck (st : State) (src : Address) (msg : Message) : State * list Packet :=
@@ -63,21 +59,21 @@ Definition procMsgWithCheck (st : State) (src : Address) (msg : Message) : State
     let: (stRB', pktsRB) := RBP.procMsgWithCheck st.(stRB) src mRB in
     let: st' := st <| stRB := stRB' |> in
     let: pkts := map pkt_inl pktsRB in
-    match triggered st.(stRB) stRB' mRB with
-    | Some (q, r, v) =>
-      let: (stAC', pktsAC) := ACP.procInt (st.(stAC) (q, r)) (ACP.SubmitIntTrans v) in
-      (st' <| stAC := map_update RBP.AddrRdPair_eqdec (q, r) stAC' st.(stAC) |>, pkts ++ map (pkt_inr (q, r)) pktsAC)
+    match triggered st.(stRB) stRB' with
+    | Some v =>
+      let: (stAC', pktsAC) := ACP.procInt st.(stAC) (ACP.SubmitIntTrans v) in
+      (st' <| stAC := stAC' |>, pkts ++ map pkt_inr pktsAC)
     | None => (st', pkts)
     end
-  | inr (ar, mAC) =>
-    let: (stAC', pktsAC) := ACP.procMsgWithCheck (st.(stAC) ar) src mAC in
-    let: pkts := map (pkt_inr ar) pktsAC in
-    (st <| stAC := map_update RBP.AddrRdPair_eqdec ar stAC' st.(stAC) |>, pkts)
+  | inr mAC =>
+    let: (stAC', pktsAC) := ACP.procMsgWithCheck st.(stAC) src mAC in
+    let: pkts := map pkt_inr pktsAC in
+    (st <| stAC := stAC' |>, pkts)
   end.
 
 End RBACProtocol.
 
-Module RBACProtocolImpl (A : NetAddr) (R : Round) (Sn : Signable) (V : SignableValue Sn) (VBFT : ValueBFT A R V) 
+Module RBACProtocolImpl (A : NetAddr) (R : Round) (ARP : AddrRoundPair A R) (Sn : Signable) (V : SignableValue Sn) (VBFT : ValueBFT A R V) 
   (BTh : ClassicByzThreshold A) (RBM : RBMessage A R V)
   (P : PKI A Sn) (TSS0 : ThresholdSignatureSchemePrim A Sn with Definition thres := BTh.t0) (* ! *)
   (TSS : ThresholdSignatureScheme A Sn with Module TSSPrim := TSS0)
@@ -88,8 +84,8 @@ Module RBACProtocolImpl (A : NetAddr) (R : Round) (Sn : Signable) (V : SignableV
   (RBP : RBProtocol A R V VBFT BTh RBM RBPk)
   (ACP : ACProtocol A Sn V BTh P TSS0 TSS ACDT CC ACM ACPk)
   (Pk : SimplePacket A M) <: Protocol A M Pk BTh
-  <: RBACProtocol A R Sn V VBFT BTh RBM P TSS0 TSS ACDT CC ACM M RBPk ACPk RBP ACP Pk.
+  <: RBACProtocol A R ARP Sn V VBFT BTh RBM P TSS0 TSS ACDT CC ACM M RBPk ACPk RBP ACP Pk.
 
-Include RBACProtocol A R Sn V VBFT BTh RBM P TSS0 TSS ACDT CC ACM M RBPk ACPk RBP ACP Pk.
+Include RBACProtocol A R ARP Sn V VBFT BTh RBM P TSS0 TSS ACDT CC ACM M RBPk ACPk RBP ACP Pk.
 
 End RBACProtocolImpl.
--- Protocols/RBABC/Types.v
+++ Protocols/RBABC/Types.v
@@ -5,6 +5,43 @@ From ABCProtocol.Structures Require Export Types.
 From ABCProtocol.Protocols.ABC Require Export Types.
 From ABCProtocol.Protocols.RB Require Export Types.
 
+(* TODO it seems that if we allow multiple instances of ABC to run at the same time, 
+    then it is possible for adversaries to collect signatures from different instances
+    and use them in one particular instance.
+  in this case, we need to change the protocol a little bit ...
+  (1) we only allow one instance of ABC for some (node, round) pair. 
+  (2) the value of ABC to be signed becomes the product of instance id and value. 
+*)
+(*
+Module Export SingleRound <: Round.
+
+Definition Round := unit.
+Definition Round_eqdec : forall r1 r2 : Round, {r1 = r2} + {r1 <> r2}.
+  intros [][]. left. reflexivity.
+Qed.
+
+End SingleRound.
+*)
+Module Type AddrRoundPair (Export A : NetAddr) (Export R : Round).
+
+Parameter arp : Address * Round.
+
+End AddrRoundPair.
+(*
+Module Type SingleValueBFT (Export A : NetAddr) (Export V : Value) (Export LS : Leadership A V) <: ValueBFT A SingleRound V.
+
+Definition value_bft : Address -> Round -> Value := fun _ _ => inputval.
+
+End SingleValueBFT.
+
+Module SingleValueBFTImpl (Export A : NetAddr) (Export V : Value) (Export LS : Leadership A V) 
+  <: ValueBFT A SingleRound V <: SingleValueBFT A V LS.
+
+Include SingleValueBFT A V LS.
+
+End SingleValueBFTImpl.
+*)
+
 Module Type RBACMessage (A : NetAddr) (R : Round) (Sn : Signable) (V : SignableValue Sn) (P : PKI A Sn) (TSS : ThresholdSignatureScheme A Sn)
   (ACDT : ACDataTypes A Sn V P TSS)
   (* module type ...!!! *)
@@ -12,11 +49,11 @@ Module Type RBACMessage (A : NetAddr) (R : Round) (Sn : Signable) (V : SignableV
 
 Import A R V P TSS ACDT.
 
-Definition Message := (RBM.Message + (Address * Round) * ACM.Message)%type.
+Definition Message := (RBM.Message + ACM.Message)%type.
 
 Definition Message_eqdec : forall (m1 m2 : Message), {m1 = m2} + {m1 <> m2}.
   intros.
-  repeat (decide equality; auto using RBM.Message_eqdec, ACM.Message_eqdec, Address_eqdec, Round_eqdec).
+  repeat (decide equality; auto using RBM.Message_eqdec, ACM.Message_eqdec).
 Qed.
 
 End RBACMessage.
