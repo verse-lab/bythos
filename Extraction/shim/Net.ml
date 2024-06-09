e82264db18b4a5570a32a823426c35aed311c09b
zqy1018 <zqy1018@sjtu.edu.cn>
2024-06-09 20:25:41 +0800

    clean up

--- Extraction/shim/Net.ml
+++ Extraction/shim/Net.ml
@@ -19,8 +19,6 @@ let rec retry_until_no_eintr f =
 (* TODO FIXME: receive all at once is one of the issues with Verdi *) (* COPIED COMMENT *)
 let send_chunk (fd : file_descr) (buf : bytes) : unit =
   let len = Bytes.length buf in
-  (* Printf.printf "sending chunk of length %d" len; print_newline (); *) (* COPIED COMMENT *)
-  (* let n = Unix.send fd (Util.raw_bytes_of_int len) 0 4 [] in *)
   let len_bytes = Bytes.create 4 in
   Bytes.set_int32_be len_bytes 0 (Int32.of_int len);
   let n = retry_until_no_eintr (fun () -> send fd len_bytes 0 4 []) in
@@ -37,17 +35,13 @@ let recv_or_close fd buf offs len flags =
   n
 
 let receive_chunk (fd : file_descr) : bytes =
-  (* let buf4 = Bytes.make 4 '\x00' in *)
   let buf4 = Bytes.create 4 in
   let n = recv_or_close fd buf4 0 4 [] in
   if n < 4 then
     failwith "receive_chunk: message header did not arrive all at once.";
-  (* let len = int_of_raw_bytes buf4 in *)
   let len = Int32.to_int (Bytes.get_int32_be buf4 0) in
-  (* let buf = Bytes.make len '\x00' in *)
   let buf = Bytes.create len in
   let n = recv_or_close fd buf 0 len [] in
-  (* Printf.printf "received chunk of length %d" len; print_newline (); *) (* COPIED COMMENT *)
   if n < len then
     failwith
         (Printf.sprintf "receive_chunk: message of length %d did not arrive all at once." len);
@@ -70,10 +64,8 @@ let get_name_for_read_fd fd =
 
 let build_connection_to ((ip, port) : address) =
   let write_fd = socket PF_INET SOCK_STREAM 0 in
-  (* let (ip, port) = get_addr_port cfg addr in *)
   let entry = gethostbyname ip in
   let node_addr = ADDR_INET (Array.get entry.h_addr_list 0, port) in
-  (* let chunk = Bytes.of_string (string_of_int cfg.me) in *)
   (* NOTE: send the IP port pair here, since it is not straightforward
       to obtain the client IP from the `node_addr` returned by `accept()` *)
   let chunk = Marshal.to_bytes (!me_ip, !me_port) [] in
@@ -96,8 +88,6 @@ let listen_fd : file_descr = socket PF_INET SOCK_STREAM 0
 (* set up the listening socket *)
 let setup () =
   Printexc.record_backtrace true;
-  (* the_cfg := Some cfg;
-  let (_, port) = get_addr_port cfg cfg.me in *)
   let port = !me_port in
   Printf.printf "listening on port %d" port; print_newline ();
   (* get around those "Address already in use" error messages *)
@@ -108,16 +98,11 @@ let setup () =
 (* process newly coming connection request *)
 let new_conn () =
   print_endline "new connection!";
-  (* let (node_fd, node_addr) = retry_until_no_eintr (fun () -> accept listen_fd) in *)
   let (node_fd, _) = retry_until_no_eintr (fun () -> accept listen_fd) in
   let chunk = receive_chunk node_fd in
-  (* let node = Bytes.to_string chunk in
-  let name = int_of_string node in *)
   let addr : address = Marshal.from_bytes chunk 0 in
   Hashtbl.add read_fds node_fd addr;
-  (* ignore (get_write_fd name); *) (* COPIED COMMENT *)
-  (* Printf.printf "done processing new connection from node %s" node; *)
-  Printf.printf "done processing new connection from node %s, %d" (fst addr) (snd addr);
+  Printf.printf "done processing new connection from node %s" (string_of_address addr);
   print_newline ()
 
 let check_for_new_connections () =
@@ -168,18 +153,6 @@ let get_pkt =
       end
     end
   in aux
-
-(*
-let rec get_pkt = function
-  | [] -> None
-  | fd :: fds ->
-      try
-        Some (recv_pkt fd)
-      with _ ->
-      begin
-        get_pkt fds
-      end
-*)
 
 let send_pkt dst pkt =
   let fd = get_write_fd dst in
